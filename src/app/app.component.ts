1a1d1284eebe249325da15a5dfa6e5609d02178b
John Case <jcase@symplr.com>
2020-06-26 17:46:43 -0500

    More buttons and datepickers and themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { purpleDarkTheme, purpleTheme } from './themeing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-webcomponent-demo';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('splitBtn') splitBtn!: ElementRef;
+  @ViewChild('customStyle') customButton!: ElementRef;
+  @ViewChild('formattedDate') formattedDatePicker!: ElementRef;
+  @ViewChild('dateWithValue') datePickerWithValue!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.customButton.nativeElement.theme = purpleTheme;
+
+    // this.splitBtn.nativeElement.theme = purpleDarkTheme;
+    this.splitBtn.nativeElement.onClick = () => { alert('main button clicked!'); };
+    this.splitBtn.nativeElement.menuProps = {
+      items: [
+        {
+          key: 'emailMessage',
+          text: 'Email message',
+          iconProps: { iconName: 'Mail' },
+        },
+        {
+          key: 'calendarEvent',
+          text: 'Calendar event',
+          iconProps: { iconName: 'Calendar' },
+        },
+      ],
+    };
+
+    this.formattedDatePicker.nativeElement.formatDate = (date) => {
+      return date.getDate() + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear() % 100);
+    };
+    this.formattedDatePicker.nativeElement.parseDateFormatString = (val) => {
+      const date = val || new Date();
+      const values = (val || '').trim().split('/');
+      const day = val.length > 0 ? Math.max(1, Math.min(31, parseInt(values[0], 10))) : date.getDate();
+      const month = val.length > 1 ? Math.max(1, Math.min(12, parseInt(values[1], 10))) - 1 : date.getMonth();
+      let year = val.length > 2 ? parseInt(values[2], 10) : date.getFullYear();
+      if (year < 100) {
+        year += date.getFullYear() - (date.getFullYear() % 100);
+      }
+      return new Date(year, month, day);
+    };
+
+    // This only works if the other elements are commented out.
+    this.formattedDatePicker.nativeElement.theme = purpleDarkTheme;
+
+    this.datePickerWithValue.nativeElement.value = new Date(1776, 6, 4);
+  }
+
 }
---
+++ src/app/themeing.ts
@@ -0,0 +1,54 @@
+import { createTheme } from '@uifabric/styling';
+
+export const purpleTheme = createTheme({
+  palette: {
+    themePrimary: '#5409ab',
+    themeLighterAlt: '#f7f2fc',
+    themeLighter: '#decdf2',
+    themeLight: '#c3a4e6',
+    themeTertiary: '#8e58cd',
+    themeSecondary: '#641eb5',
+    themeDarkAlt: '#4c089a',
+    themeDark: '#400682',
+    themeDarker: '#2f0560',
+    neutralLighterAlt: '#faf9f8',
+    neutralLighter: '#f3f2f1',
+    neutralLight: '#edebe9',
+    neutralQuaternaryAlt: '#e1dfdd',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c6c4',
+    neutralTertiary: '#595857',
+    neutralSecondary: '#373636',
+    neutralPrimaryAlt: '#2f2e2e',
+    neutralPrimary: '#000000',
+    neutralDark: '#151514',
+    black: '#0b0b0b',
+    white: '#ffffff',
+  }
+  });
+
+export const purpleDarkTheme = createTheme({
+  palette: {
+    themePrimary: '#8f65bf',
+    themeLighterAlt: '#060408',
+    themeLighter: '#17101f',
+    themeLight: '#2b1e39',
+    themeTertiary: '#563d73',
+    themeSecondary: '#7e59a8',
+    themeDarkAlt: '#9972c6',
+    themeDark: '#a785cf',
+    themeDarker: '#bda2db',
+    neutralLighterAlt: '#0b0b0b',
+    neutralLighter: '#151515',
+    neutralLight: '#252525',
+    neutralQuaternaryAlt: '#2f2f2f',
+    neutralQuaternary: '#373737',
+    neutralTertiaryAlt: '#595859',
+    neutralTertiary: '#c8c8c8',
+    neutralSecondary: '#d0d0d0',
+    neutralPrimaryAlt: '#dadada',
+    neutralPrimary: '#ffffff',
+    neutralDark: '#f4f4f4',
+    black: '#f8f8f8',
+    white: '#000000',
+  }});
